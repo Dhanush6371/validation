160871983c4722c283771fba18ff1c6567168c86
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2022-08-13 16:40:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+let text=document.getElementById("text");
+let form=document.querySelector("form");
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(text){
+   return /^(([0-5]+[a-zA-Z\-0-9]+\.)+[a-zA-Z])))$/.test(text);
+}
+
+
+
